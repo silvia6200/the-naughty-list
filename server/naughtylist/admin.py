91941b8bca53c5d6664c8add253421cb1de0d362
LinasKo <linaskondrackis@gmail.com>
2014-07-03 22:27:00 +0300

    added basic voice editing functionality to admin, with bootstrap

--- server/naughtylist/admin.py
+++ server/naughtylist/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from naughtylist.models import Voice
+from django.forms import TextInput, Textarea
+from django.db import models
 
-# Register your models here.
+class VoiceAdmin(admin.ModelAdmin):
+    list_display = (Voice._meta.get_all_field_names())
+    
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows':4})},
+    }
+
+
+admin.site.register(Voice, VoiceAdmin)
