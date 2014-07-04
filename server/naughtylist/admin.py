fa17506c89a59de88df00d3d2c6587d55e010078
LinasKo <linaskondrackis@gmail.com>
2014-07-04 17:22:15 +0300

    Changed order of fields. Added admin action that toggles visibility of non-fuzzy voices. Select at least 1 voice before using this action

--- server/naughtylist/admin.py
+++ server/naughtylist/admin.py
@@ -4,11 +4,26 @@ from django.forms import TextInput, Textarea
 from django.db import models
 
 class VoiceAdmin(admin.ModelAdmin):
-    list_display = (Voice._meta.get_all_field_names())
+	
+	SHOW_FUZZY = True
+	
+	def toggle_fuzzy(modeladmin, request, queryset):
+		VoiceAdmin.SHOW_FUZZY = not VoiceAdmin.SHOW_FUZZY
+	toggle_fuzzy.short_description = "Toggle visibility of non-fuzzy voices. Select at least 1 voice before using this."
+	
+	list_display = ('datetime_added', 'user', 'offender', 'reason', 'message', 'naughty', 'fuzzy', 'verified')
     
-    formfield_overrides = {
-        models.TextField: {'widget': Textarea(attrs={'rows':4})},
-    }
-
+	formfield_overrides = {
+		models.TextField: {'widget': Textarea(attrs={'rows':4})},
+	}	
+	
+	actions = [toggle_fuzzy]
+    
+	def queryset(self, request):
+		qs = super(VoiceAdmin, self).queryset(request)
+		if VoiceAdmin.SHOW_FUZZY:
+			return qs
+		else:
+			return qs.filter(fuzzy=True)
 
 admin.site.register(Voice, VoiceAdmin)
